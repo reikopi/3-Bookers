0a6e1966db8efebf5649f009f9e6e48867c2a781
EC2 Default User <ec2-user@ip-172-31-42-80.ec2.internal>
2022-06-05 07:45:48 +0000

    [Add]3-Bookers_モデル作成後、サーバーを起動しようとしたが、Blocked hostのエラーが出たため、development.rbを修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.hosts << "0e4793c777d04be5baa5a7293beaf84a.vfs.cloud9.us-east-1.amazonaws.com"
+  # Blocked hostエラーが出たので追加した。
 end
