c04daf45d5de547d2100740fc66aefe8ff9234bb
EC2 Default User <ec2-user@ip-172-31-42-80.ec2.internal>
2022-06-05 08:48:24 +0000

    [Add]3-Bookers_ファイルの作成、ファイルの設定

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,8 @@
 ActiveRecord::Schema.define(version: 2022_06_05_072513) do
 
   create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
