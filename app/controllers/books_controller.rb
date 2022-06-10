2b0ff45fee11ccb856f01dd6c5ab82442ee2a0ba
EC2 Default User <ec2-user@ip-172-31-42-80.ec2.internal>
2022-06-10 13:25:10 +0000

    [Add]3-Bookers_Edit改善及びCss修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,13 +26,13 @@ class BooksController < ApplicationController
   end
   
   def update
-      book =Book.find(params [:id] )
+      book = Book.find(params[:id])
       book.update(book_params)
       redirect_to book_path(book.id)
   end
   
   def destroy
-    book= Book.find(params [:id])
+    book= Book.find(params[:id])
     book.destroy
     redirect_to 'books/index'
   end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get '/top' => 'homes#top'
   post 'books' => 'books#create'
   get 'books/:id' => 'books#show', as:'book'
-  patch 'book/:id' => 'books#update', as:'update_book'
+  patch 'books/:id' => 'books#update', as:'update_book'
   delete 'books/:id' => 'books#destroy', as:'destroy_book'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
