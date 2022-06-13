46ba4c67a2cfb3ff9db0ee172a4851e25fe2700e
EC2 Default User <ec2-user@ip-172-31-42-80.ec2.internal>
2022-06-13 12:44:21 +0000

    [Finish]3-bookers_修正追記

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  root to: "homes#top"
   get 'books/new'
-  get 'books/index' => 'books#index', as: 'index_book'
+  get '/books' => 'books#index', as: 'index_book'
   get 'books/:id/edit' => 'books#edit', as:'edit_book'
   get '/top' => 'homes#top'
   post 'books' => 'books#create'
